7477b1840ec90ff6cdc113424a8e9f9d298ab37c
arian321 <74503623+arian321@users.noreply.github.com>
2024-02-10 10:24:31 -0500

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -18,7 +18,7 @@ logger.setLevel(logging.DEBUG)
 # Now using a new TikTok library https://github.com/Russell-Newton/TikTokPy
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+# ghPagesURL = "https://arian321.github.io/tiktok-rss-flat/"
 
 # Custom Domain
 ghPagesURL = "https://tiktokrss.conoroneill.com/"
